8893ce77dce98f3e3fe94f443e4b3c1257967be4
Philipp Dargel <chisui.pd@gmail.com>
2021-09-02 19:51:00 +0200

    more refactoring

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,5 @@
-use std::io::{self, Read, ErrorKind, Result};
-use std::convert::From;
-use std::str;
+use std::io::{self, ErrorKind, Read};
+
 use thiserror::Error;
 
 
@@ -10,78 +9,109 @@ pub enum AcfToken {
     DictStart,
     DictEnd,
 }
+
 #[derive(Debug, Error)]
-pub enum TokenError {
+pub enum ParseError {
     #[error("Unexpected Character '{0:?}'")]
     UnexpectedCharacter(char),
-    #[error("Unexpected Token {0:?}")]
-    UnexpectedToken(AcfToken),
     #[error("Unterminated String literal")]
     UnterminatedString,
-    #[error("Path not found {0:?}")]
-    PathNotFound(Vec<String>),
-}
-impl From<TokenError> for io::Error {
-    fn from(err: TokenError) -> Self {
-        io::Error::new(ErrorKind::InvalidData, err)
-    }
 }
 
-pub struct AcfTokenStream<R: Read>{
+fn format_path(items: impl IntoIterator<Item = impl AsRef<str>>) -> String {
+    let mut s = String::new();
+    for p in items {
+        s.push('.');
+        s.push_str(p.as_ref());
+    }
+    s
+}
+
+#[derive(Debug, Error)]
+pub enum StreamError {
+    #[error("Generic I/O error: {0}")]
+    Io(#[from] io::Error),
+    #[error("Invalid data: {0}")]
+    Parse(#[from] ParseError),
+    #[error("Unexpected Token {0:?}")]
+    UnexpectedToken(AcfToken),
+    #[error("Path not found {}", format_path(&.0[..]))]
+    PathNotFound(Vec<String>),
+}
+
+type Res<A> = Result<A, StreamError>;
+
+pub struct AcfTokenStream<R> {
     read: R,
     depth: i64,
 }
 impl<R: Read> AcfTokenStream<R> {
     pub fn new(read: R) -> Self {
-        Self {
-            read,
-            depth: 0,
-        }
+        Self { read, depth: 0 }
     }
-    pub fn try_next(&mut self) -> Result<Option<AcfToken>> {
-        match next_non_whitespace_char(&mut self.read)? {
+
+    pub fn depth(&self) -> i64 {
+        self.depth
+    }
+
+    pub fn try_next(&mut self) -> Res<Option<AcfToken>> {
+        Ok(match next_non_whitespace_char(&mut self.read)? {
             Some('{') => {
                 self.depth += 1;
-                Ok(Some(AcfToken::DictStart))
+                Some(AcfToken::DictStart)
             }
             Some('}') => {
                 self.depth -= 1;
-                Ok(Some(AcfToken::DictEnd))
+                Some(AcfToken::DictEnd)
             }
-            Some('"') => parse_str(&mut self.read)
-                .map(|o| o.map(AcfToken::String)),
-            Some(c) => Err(TokenError::UnexpectedCharacter(c).into()),
-            None => Ok(None)
-        }
+            Some('"') => {
+                let s = parse_str(&mut self.read)?;
+                s.map(AcfToken::String)
+            }
+            Some(c) => {
+                Err(StreamError::from(ParseError::UnexpectedCharacter(c)))?
+            },
+            None => None,
+        })
     }
-    pub fn expect_next(&mut self) -> Result<AcfToken> {
-        let t = self.try_next()?;
-        t.ok_or(io::Error::new(ErrorKind::UnexpectedEof, ""))
+
+    pub fn expect_next(&mut self) -> Res<AcfToken> {
+        self.try_next()?
+            .ok_or(StreamError::from(io::Error::new(ErrorKind::UnexpectedEof, "")))
     }
-    pub fn expect(&mut self, token: AcfToken) -> Result<()> {
+
+    pub fn expect(&mut self, token: AcfToken) -> Res<()> {
         let t = self.expect_next()?;
         if t == token {
             Ok(())
         } else {
-            Err(TokenError::UnexpectedToken(t).into())
+            Err(StreamError::UnexpectedToken(t))
         }
     }
-    pub fn select(&mut self, target: impl AsRef<str>) -> Result<Option<()>> {
+
+    pub fn select(&mut self, target: impl AsRef<str>) -> Res<Option<()>> {
         while let Some(t) = self.try_next()? {
             match t {
-                AcfToken::String(field) => if field == target.as_ref() {
-                    return Ok(Some(()))
+                AcfToken::String(field) => {
+                    if field == target.as_ref() {
+                        return Ok(Some(()));
+                    }
                 }
-                AcfToken::DictEnd => { break; }
-                AcfToken::DictStart => { self.close_dict()?; }
+                AcfToken::DictEnd => {
+                    break;
+                }
+                AcfToken::DictStart => {
+                    self.close_dict()?;
+                }
             }
         }
         Ok(None)
     }
+
     pub fn try_select_path(
         &mut self,
         path: impl IntoIterator<Item = impl AsRef<str>>,
-    ) -> Result<Option<()>> {
+    ) -> Res<Option<()>> {
         let mut not_first = false;
         for field in path {
             if not_first {
@@ -89,76 +119,69 @@ impl<R: Read> AcfTokenStream<R> {
             } else {
                 not_first = true;
             }
-            match self.select(field)? {
-                None => { return Ok(None) }
-                Some(_) => {}
+            if self.select(field)?.is_none() {
+                return Ok(None);
             }
         }
         Ok(Some(()))
     }
-    pub fn select_path(
-        &mut self,
-        path: impl IntoIterator<Item = impl AsRef<str>>,
-    ) -> Result<()> {
-        let path_vec: Vec<String> = path.into_iter()
-                .map(|s| s.as_ref().to_owned())
-                .collect();
-        let r = self.try_select_path(path_vec.clone())?;
-        r.ok_or_else(|| io::Error::from(TokenError::PathNotFound(path_vec)))
+
+    pub fn select_path<'a, T, U: AsRef<str>>(&mut self, path: &'a T) -> Res<()>
+    where
+        &'a T: IntoIterator<Item = U>,
+    {
+        self.try_select_path(path)?.ok_or_else(|| {
+            let owned = path.into_iter().map(|s| s.as_ref().to_owned()).collect();
+            StreamError::PathNotFound(owned)
+        })
     }
-    pub fn close_dict(&mut self) -> Result<()> {
+
+    pub fn close_dict(&mut self) -> Res<()> {
         self.skip_to_depth(self.depth - 1)
     }
-    fn skip_to_depth(&mut self, target_depth: i64) -> Result<()> {
-        while let Some(_) = self.try_next()? {
-            if self.depth == target_depth {
-                break;
-            }
+
+    fn skip_to_depth(&mut self, target_depth: i64) -> Res<()> {
+        while self.depth > target_depth { 
+            self.expect_next()?;
         }
         Ok(())
     }
 }
-fn next_char<R: Read>(mut reader: R) -> Result<Option<char>> {
+
+// TODO: handle UTF-8 better, possibly by making this work with bytes and letting parse_str handle it
+fn next_char<R: Read>(mut reader: R) -> io::Result<Option<char>> {
     let mut buf: [u8; 1] = [0];
-    let size = reader.read(&mut buf)?;
-    Ok(if size == 1 {
+    Ok(if reader.read(&mut buf)? == 1 {
         Some(buf[0] as char)
     } else {
         None
     })
 }
-fn next_non_whitespace_char<R: Read>(mut reader: R) -> Result<Option<char>> {
+
+fn next_non_whitespace_char<R: Read>(mut reader: R) -> io::Result<Option<char>> {
+    while let Some(c) = next_char(&mut reader)? {
+        if !c.is_whitespace() {
+            return Ok(Some(c));
+        }
+    }
+    Ok(None)
+}
+
+fn parse_str<R: Read>(mut reader: R) -> Res<Option<String>> {
+    let mut buf = String::new();
     loop {
         match next_char(&mut reader)? {
-            Some(c) => if !c.is_whitespace() {
-                return Ok(Some(c))
-            },
-            None => { return Ok(None) }
+            Some('"') => return Ok(Some(buf)),
+            // TODO: handle escape sequences?
+            Some(c) => buf.push(c),
+            None => return Err(StreamError::from(ParseError::UnterminatedString)),
         }
     }
 }
-fn parse_str<R: Read>(mut reader: R) -> Result<Option<String>> {
-    let mut buf: Vec<u8> = vec![];
-    loop {
-        match next_char(&mut reader)? {
-            Some('"') => {
-                return str::from_utf8(&buf)
-                    .map_err(|e| io::Error::new(ErrorKind::Other, e))
-                    .map(str::to_owned)
-                    .map(Some);
-            }
-            // TODO: handle escape sequences and utf8?
-            Some(c) => buf.push(c as u8),
-            None => { return Err(TokenError::UnterminatedString.into()); }
-        }
-    }
-}
+
 impl<R: Read> Iterator for AcfTokenStream<R> {
-    type Item = Result<AcfToken>;
+    type Item = Res<AcfToken>;
     fn next(&mut self) -> Option<Self::Item> {
-        match self.try_next() {
-            Ok(t)  => t.map(Ok),
-            Err(e) => Some(Err(e)),
-        }
+        self.try_next().transpose()
     }
 }
--- steamlaunch/src/main.rs
+++ steamlaunch/src/main.rs
@@ -1,16 +1,26 @@
-use std::io::{Error, ErrorKind, Result};
-use std::collections::HashMap;
-use std::array::IntoIter;
+use std::io;
 use std::env;
 use std::str;
-use std::fmt;
-use std::error;
+use std::collections::HashMap;
 use std::process::{Command, Stdio};
 use std::fs::File;
 use std::path::PathBuf;
-use structopt::StructOpt;
-use steamacf::{AcfTokenStream, AcfToken, TokenError};
 
+use structopt::StructOpt;
+use thiserror::Error;
+use steamacf::{AcfTokenStream, AcfToken, StreamError};
+
+
+#[derive(Debug, Error)]
+pub enum SteamLaunchError {
+    #[error("Steam file malformed: {0}")]
+    Stream(#[from] StreamError),
+    #[error("Generic I/O error: {0}")]
+    Io(#[from] io::Error),
+    #[error("password command failed unexpectedly: {0:?}")]
+    PasswordCmdFailed(String),
+}
+type Res<A> = Result<A, SteamLaunchError>;
 
 #[derive(Debug, StructOpt)]
 #[structopt(
@@ -32,7 +42,7 @@ struct SteamLaunchArgs {
     cmd: SteamLaunchCmds
 }
 impl SteamLaunchArgs {
-    fn exec(&self) -> Result<i32> {
+    fn exec(&self) -> Res<i32> {
         let steam_dir = steam_dir(&self.steam_dir)?;
         
         self.cmd.exec(SteamLaunchCtx {
@@ -43,17 +53,7 @@ impl SteamLaunchArgs {
         })
     }
 }
-#[derive(Debug)]
-struct PasswordCmdFailed(Vec<u8>);
-impl error::Error for PasswordCmdFailed {}
-impl fmt::Display for PasswordCmdFailed {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "password command failed unexpectedly:")?;
-        let stderr = str::from_utf8(&self.0)
-            .map_err(|_| fmt::Error)?;
-        write!(f, "{}", stderr)
-    }
-}
+
 struct SteamLaunchCtx {
     steam_dir: PathBuf,
     user: Option<String>,
@@ -61,7 +61,7 @@ struct SteamLaunchCtx {
     password_cmd: Option<String>,
 }
 impl SteamLaunchCtx {
-    fn login(&self) -> Result<Option<(String, String)>> {
+    fn login(&self) -> Res<Option<(String, String)>> {
         match &self.user {
             Some(user) => {
                 let password = self.password()?;
@@ -73,28 +73,31 @@ impl SteamLaunchCtx {
             None => Ok(None)
         }
     }
-    fn password(&self) -> Result<Option<String>> {
+    fn password(&self) -> Res<Option<String>> {
         match &self.password_cmd {
             Some(cmd) => {
                 let output = Command::new(cmd).output()?;
                 if let Some(_) = output.status.code() {
-                    Err(Error::new(ErrorKind::Other, PasswordCmdFailed(output.stderr)))
+                    Err(match str::from_utf8(&output.stderr) {
+                        Ok(err) => SteamLaunchError::PasswordCmdFailed(err.to_string()),
+                        Err(_)  => SteamLaunchError::PasswordCmdFailed("failed with unreadable error message".to_string()),
+                    })
                 } else {
                     let raw = str::from_utf8(&output.stdout)
-                        .map_err(|_| Error::new(ErrorKind::Other,PasswordCmdFailed("malformed output".as_bytes().to_vec())))?;
+                        .map_err(|_| SteamLaunchError::PasswordCmdFailed("malformed output".to_string()))?;
                     Ok(Some(raw.trim().to_owned()))
                 }
             },
             None => Ok(self.password.clone()),
         }
     }
-    fn load_registry(&self) -> Result<SteamRegistry> {
+    fn load_registry(&self) -> Res<SteamRegistry> {
         let mut reg_file = self.steam_dir.clone();
         reg_file.push("registry.vdf");
         
         let f = File::open(reg_file)?;
         let mut tokens = AcfTokenStream::new(f);
-        tokens.select_path(IntoIter::new(["Registry", "HKCU", "Software", "Valve", "Steam", "Apps"]))?;
+        tokens.select_path(&["Registry", "HKCU", "Software", "Valve", "Steam", "Apps"])?;
 
         tokens.expect(AcfToken::DictStart)?;
         let mut reg = HashMap::new();
@@ -103,7 +106,7 @@ impl SteamLaunchCtx {
             if let Some(_) = tokens.select("name")? {
                 match tokens.expect_next()? {
                     AcfToken::String(name) => { reg.insert(name, id); },
-                    t => { return Err(TokenError::UnexpectedToken(t).into()); },
+                    t => { return Err(SteamLaunchError::from(StreamError::UnexpectedToken(t))); },
                 }
                 tokens.close_dict()?;
             }
@@ -121,10 +124,10 @@ enum SteamLaunchCmds {
     Completion(CompletionCmd),
 }
 trait Cmd {
-    fn exec(&self, ctx: SteamLaunchCtx) -> Result<i32>;
+    fn exec(&self, ctx: SteamLaunchCtx) -> Res<i32>;
 }
 impl Cmd for SteamLaunchCmds {
-    fn exec(&self, ctx: SteamLaunchCtx) -> Result<i32> {
+    fn exec(&self, ctx: SteamLaunchCtx) -> Res<i32> {
         match self {
             SteamLaunchCmds::Start(cmd)      => cmd.exec(ctx),
             SteamLaunchCmds::List(cmd)       => cmd.exec(ctx),
@@ -139,7 +142,7 @@ enum CompletionCmd {
     Zsh,
 }
 impl Cmd for CompletionCmd {
-    fn exec(&self, _: SteamLaunchCtx) -> Result<i32> {
+    fn exec(&self, _: SteamLaunchCtx) -> Res<i32> {
         println!("{:?}", self);
         Ok(0)
     }
@@ -148,7 +151,7 @@ impl Cmd for CompletionCmd {
 #[derive(Debug, StructOpt)]
 struct ListCmd {}
 impl Cmd for ListCmd {
-    fn exec(&self, ctx: SteamLaunchCtx) -> Result<i32> {        
+    fn exec(&self, ctx: SteamLaunchCtx) -> Res<i32> {        
         let registry = ctx.load_registry()?;
         print!("{:?}", registry);
         Ok(0)
@@ -167,11 +170,11 @@ struct StartCmd {
     args: Vec<String>,
 }
 impl Cmd for StartCmd {
-    fn exec(&self, ctx: SteamLaunchCtx) -> Result<i32> {
+    fn exec(&self, ctx: SteamLaunchCtx) -> Res<i32> {
         let app_id = self.app.clone();
         let mut cmd = Command::new("echo");
         if let Some((user, password)) = ctx.login()? {
-            cmd.args(["-login", user.as_str(), password.as_str()]);
+            cmd.args(&["-login", user.as_str(), password.as_str()]);
         }
         let out = cmd
             .arg("-applaunch")
@@ -185,7 +188,7 @@ impl Cmd for StartCmd {
     }
 }
 
-fn steam_dir(steam_dir: &Option<PathBuf>) -> Result<PathBuf> {
+fn steam_dir(steam_dir: &Option<PathBuf>) -> Res<PathBuf> {
     steam_dir.clone()
         .or_else(|| env::var("STEAM_DIR").ok().map(PathBuf::from))
         .or_else(|| {
@@ -194,11 +197,15 @@ fn steam_dir(steam_dir: &Option<PathBuf>) -> Result<PathBuf> {
                 .map(PathBuf::from)?;
             path.push(".steam");
             Some(path)
-        }).ok_or(Error::new(ErrorKind::NotFound, "Environment varibale HOME, not defined."))
+        }).ok_or(io::Error::new(io::ErrorKind::NotFound, "Environment varibale HOME, not defined.").into())
 }
 
-fn main() -> Result<()> {
-    let status = SteamLaunchArgs::from_args()
-        .exec()?;
-    std::process::exit(status)
+fn main() {
+    std::process::exit(match SteamLaunchArgs::from_args().exec() {
+        Ok(status) => status,
+        Err(e) => {
+            println!("{:?}", e);
+            1
+        }
+    });
 }
